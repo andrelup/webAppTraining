f61ac366c2e94c99ea9562c14a3a0430fb3c813b
andrelp <andrelcop@gmail.com>
2019-04-30 16:14:53 +0200

    Inject a service into another service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
-import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 	private recipes: Recipe [] = [
@@ -12,7 +15,7 @@ export class RecipeService {
 			[
 				new Ingredient('Meat', 1),
 				new Ingredient('French Fries', 20)
-			]),			
+			]),
 		new Recipe(
 			'Big Fat Burger',
 			'What else you need to say?',
@@ -22,7 +25,15 @@ export class RecipeService {
 				new Ingredient('Meat', 1)
 			])
 	  ];
+
+	constructor(private slService: ShoppingListService) {
+
+	}
 	getRecipes() {
 		return this.recipes.slice();
+	}
+
+	addIngredientsToShoppingList(ingredients: Ingredient []) {
+		this.slService.addIngredients(ingredients);
 	}
 }
