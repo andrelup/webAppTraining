5aa26a29c82746e17a3e5683a7e888cc9081f5cd
andrelp <andrelcop@gmail.com>
2019-07-15 18:25:40 +0200

    add firebase. Save and fetch data method implemented.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
 
+import {DataStorageService} from '../shared/data-storage.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  public constructor(private dataStorageService: DataStorageService) {}
+
+  saveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  fetchData() {
+    this.dataStorageService.fetchRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,24 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Recipe} from '../recipes/recipe.model';
+import {RecipeService} from '../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
+  constructor(private http: HttpClient, private recipesService: RecipeService) { }
 
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http.put('https://ng-courser-recipe-book.firebaseio.com/recipes.json', recipes).subscribe(response => {
+      console.log(response);
+    });
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-courser-recipe-book.firebaseio.com/recipes.json').subscribe(recipes => {
+      this.recipesService.setRecipes(recipes);
+    });
+  }
 }
