c6f61efad8cc8942081c5711fe05d4aaadaafed9
andrelp <andrelcop@gmail.com>
2019-08-16 18:54:10 +0200

    Sign Up a user. Configure firebase to work with my app. Add spinnerComponent

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,19 +1,38 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
-  isLoginMode = true;
+  isLogingMode = true;
+  isLoading = false;
+
+  constructor(private authService: AuthService) {}
 
   onSwitchMode() {
-    this.isLoginMode = !this.isLoginMode;
+    this.isLogingMode = !this.isLogingMode;
   }
 
   onSubmit(authForm: NgForm) {
-    console.log(authForm.value);
+    if (!authForm.valid) {
+      return;
+    }
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+    this.isLoading = true;
+    if (this.isLogingMode) {
+    } else {
+      this.authService.signup(email, password).subscribe( resData => {
+        console.log(resData);
+        this.isLoading = false;
+      }, error => {
+        console.log(error);
+        this.isLoading = false;
+      });
+    }
     authForm.resetForm();
   }
 }
